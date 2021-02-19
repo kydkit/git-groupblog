56dc3058e778a5315993cb621fb7af586370d594
Hitomi <hitomi.winberg@gmail.com>
2021-02-19 14:32:38 +0100

    Added an posts array to Blogcontext.js

--- src/Contexts/BlogContext.js
+++ src/Contexts/BlogContext.js
@@ -1,9 +1,31 @@
-import {createContext} from "react"
+import {createContext, useState} from "react"
 export const BlogContext =createContext();
 function BlogContextProvider(props) {
+  const[posts, setPosts]=useState([
+    {
+        header:"Boil Potatoes",
+        content:"Boil Potatoes",
+        like:0
+    },
+    {
+        header:"Boil Carrots",
+        content:"Boil Carrots",
+        like:0
+    },
+    {
+        header:"Bake Potatoes",
+        content:"Bake Potatoes",
+        like:0
+    },
+  ]);
+
+  const values ={
+    posts,
+  }
+  
   return (
     <div>
-      <BlogContext.Provider>
+      <BlogContext.Provider value={values}>
         {props.children}
       </BlogContext.Provider>
     </div>
